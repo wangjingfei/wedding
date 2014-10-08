c5f423a14abdafa074442abec78e5e61441596b5
Wang Jingfei <wangjingfei@live.com>
2014-10-08 09:44:18 +0000

    Add password.

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://@localhost/mean-prod',
+  db: 'mongodb://thu:thu@localhost/mean-dev2',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
